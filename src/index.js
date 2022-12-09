7c7c305cd2dbbc0db300178eb8eeb17d426827d8
USERSAD\tferrech <teresacharneca@gmail.com>
2022-12-09 15:57:05 +0000

    Adding new exercise

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import './index.css';
-import GameList from './GameList';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<GameList />);
